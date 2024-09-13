e211640a306f473f57233becff4305fe46595352
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-09-13 16:55:02 +0100

    création de logique et d'affichage et d'ajoute d'un ressource

--- ConstructionXpert/src/app/ressource/manage-ressources/manage-ressources.component.ts
+++ ConstructionXpert/src/app/ressource/manage-ressources/manage-ressources.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Ressource} from "../../models/ressource.model";
+import {RessourceService} from "../../services/ressource.service";
 
 @Component({
   selector: 'app-manage-ressources',
   templateUrl: './manage-ressources.component.html',
   styleUrls: ['./manage-ressources.component.css']
 })
-export class ManageRessourcesComponent {
+export class ManageRessourcesComponent implements OnInit{
+  ressources :Ressource[]=[];
+  displayedColumns: string[] = ['idRessource', 'nom', 'type', 'quantite', 'fournisseur', 'idTache'];
 
+
+  constructor(private services:RessourceService) {}
+  onLoading(): void {
+    this.services.recupererTouts().subscribe(
+      (response: Ressource[]) => {
+        this.ressources = response;
+        console.log('Succès', response);
+      },
+      (error) => {
+        console.log('Not found', error);
+      }
+    );
+  }
+
+  ngOnInit(): void {
+    this.onLoading();
+  }
+
+  deleteRessource(idRessource: number) {
+    this.services.deleteRessource(idRessource).subscribe(
+      () => {
+        this.ressources = this.ressources.filter(ressource => ressource.idRessource !== idRessource);
+        console.log('Ressource supprimée avec succès');
+      },
+      (error) => {
+        console.log('Erreur lors de la suppression de la ressource', error);
+      }
+    );
+  }
 }
